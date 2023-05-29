e4e2d57a62c0969b080f46d78b4fbb7ed7df045c
johnverz22 <johnny.verzola@gmail.com>
2023-05-29 22:02:26 +0800

    order, product, checkout,cart

---
+++ database/migrations/2023_05_29_131306_create_orders_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string("order_num")->nullable();
+            $table->foreignId("user_id")->constrained()->onDelete("cascade");
+            $table->decimal("total_amount", 10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
+use Illuminate\Foundation\Auth\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,22 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $user = User::factory()->create([
+            'name'=>'John Doe',
+            'email'=>'john@gmail.com',
+            'password'=>bcrypt("abcd"),
+        ]);
+        Product::factory()->create([
+            'name'=>'Nike Shoes',
+            'seller_id'=>$user->id(),
+            'price'=>5000.99,
+            'description' => 'Size: 40, 41',
+        ]);
+        Product::factory()->create([
+            'name'=>'Vans Shoes',
+            'seller_id'=>$user->id(),
+            'price'=>4000.99,
+            'description' => 'Size: 40, 41',
+        ]);
     }
 }
